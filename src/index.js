eaeae455fb6b30cd1055d023a0d17b23eb546816
ghiliweld <ghiliaweld@gmail.com>
2018-12-25 17:34:03 -0500

    fixed typeDefs and schema

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 const { GraphQLServer } = require('graphql-yoga');
 
 const initResolvers = require('./resolvers.js');
+const typeDefs = require('./typedefs/index.js');
 
 async function init() {
   const resolvers = await initResolvers().catch(e => console.log(e));
 
   const server = new GraphQLServer({
-    typeDefs: 'schema.graphql',
+    typeDefs: typeDefs,
     resolvers: resolvers
   });
 
-  server.start(() => console.log(`The server is running on http://localhost:4000`));
+  server.start({ port: 8000 }, ({ port }) => console.log(`Server started, listening on port ${port} for incoming requests.`));
 };
 
 init().catch(e => console.log(e));
