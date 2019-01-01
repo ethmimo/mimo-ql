aeea81ad6d4725acac47dfe53e1f535a55a20602
ghiliweld <ghiliaweld@gmail.com>
2019-01-01 21:27:59 +0900

    replaced some files with new npm libs

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,22 +1,22 @@
-const loadDB = require('./db.js');
+const loadIpfs = require('./ipfs.js');
 const Web3 = require('web3');
-const Mimo = require('./mimo.js');
+const initMimo = require('ethmimo');
 
 const setupResolvers = db, web3 => {
   const resolvers = {
     Query: {
-      description: () => 'Mimo GraphQL API',
-      db_address: () => Mimo.address(db),
-      getProfile: async (_, { id }) => Mimo.getProfile(db, id),
-      getProfiles: async (_, { ids }) => Mimo.getProfiles(db, ids),
-      allProfiles: async (_, { }) => Mimo.allProfiles(db, id),
-      getProfilesByName: async (_, { name }) => Mimo.getProfilesByName(db, name),
-      getProfileByEmoji: async (_, { emojis }) => Mimo.getProfileByEmoji(db, emojis),
-      isNameRegistered: async (_, { name }) => Mimo.isNameRegistered(db, name),
-      resolveENSName: async (_, { ensname }) => Mimo.isNameRegistered(db, web3, ensname)
+      description: () => 'db GraphQL API',
+      db_address: () => db.address,
+      getProfile: async (_, { id }) => db.getProfile(id),
+      getProfiles: async (_, { ids }) => db.getProfiles(ids),
+      allProfiles: async (_, { }) => db.allProfiles(id),
+      getProfilesByName: async (_, { name }) => db.getProfilesByName(name),
+      getProfileByEmoji: async (_, { emojis }) => db.getProfileByEmoji(emojis),
+      isNameRegistered: async (_, { name }) => db.isNameRegistered(name),
+      resolveENSName: async (_, { ensname }) => db.isNameRegistered(web3, ensname)
     },
     Mutation: {
-      updateProfile: async (_, { signature, data }) => Mimo.updateProfile(db, signature, data)
+      updateProfile: async (_, { signature, data }) => db.updateProfile(signature, data)
     },
     Profile: {
       id: (root) => root.id,
@@ -31,9 +31,11 @@ const setupResolvers = db, web3 => {
 // this function will load a db and initiate our resolvers to be exported
 async function initResolvers(web3 = {}) {
 
-  let resolvers = await loadDB().then(db => setupResolvers(db, web3)).catch(e => console.log(e));
-
+  const ipfs = await loadIpfs();
+  const db = await initMimo(ipfs);
+  const resolvers = setupResolvers(db, web3);
   return resolvers;
+
 };
 
 module.exports = initResolvers;
